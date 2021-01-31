991eb04da6d84dacf593a29a0657fe9aea50e15b
Kabilesh <kabilesh.ravi@cielocosta.com>
2021-01-31 16:22:20 +0530

    change till second page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { Route } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login/Login';
-import ProfileForm from './components/studentProfileForm/profileForm';
-import {BrowserRouter as Router,Switch} from "react-router-dom";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Student/Login/Login";
+import ProfileForm from "./components/Student/studentProfileForm/profileForm";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   localStorage.clear();
-}
+};
 
 function App() {
   return (
     <div className="App">
-    
-    <Router>
-    <Switch>
-      <Route exact path="/"><Login/></Route>
-      <Route path='/profile/:id'><ProfileForm/></Route>
-    </Switch>
-    </Router>
-    
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/profile/:id">
+            <ProfileForm />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
