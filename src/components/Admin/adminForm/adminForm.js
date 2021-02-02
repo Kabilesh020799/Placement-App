8c553524f18c9682460295029873eb3986bfa1bb
Kabilesh <kabilesh.ravi@cielocosta.com>
2021-02-02 15:46:59 +0530

    admin changes

--- src/components/Admin/adminForm/adminForm.js
+++ src/components/Admin/adminForm/adminForm.js
@@ -1,32 +1,43 @@
+import Axios from "axios";
 import React, { useState } from "react";
+
+import { useParams } from "react-router-dom";
 
 import "./adminForm.css";
 
 function AdminForm() {
-  const [name, setName] = useState("");
   const [role, setRole] = useState("");
   const [ctc, setCtc] = useState("");
   const [date, setDate] = useState("");
   const [cgpa, setCgpa] = useState("");
 
-  const onSubmitted = () => {};
+  const { id } = useParams();
+
+  const onSubmitted = async (e) => {
+    e.preventDefault();
+    const body = {
+      role,
+      ctc,
+      cgpa,
+      date,
+      company_id: id,
+    };
+    const token = localStorage.getItem("adminToken");
+
+    const headers = {
+      authorization: "Bearer " + token,
+    };
+    await Axios.post("http://localhost:5000/api/adminform", body, {
+      headers,
+    }).then((res) => console.log(res));
+  };
 
   return (
     <section className="admin__formdetails">
       <div className="admin__box1">
         <h1 className="admin__heading1">Company Details</h1>
         <div className="admin__details">
-          <form name="cmpy" onSubmit={onSubmitted}>
-            <input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              placeholder="Name of the company"
-              name="company_name"
-              maxLength="30"
-              className="admin__input"
-            />
-            <br />
+          <form name="cmpy">
             <input
               value={role}
               onChange={(e) => setRole(e.target.value)}
@@ -65,7 +76,7 @@ function AdminForm() {
               className="admin__input"
             />
             <br />
-            <button className="adminBtn" type="submit">
+            <button onClick={onSubmitted} className="adminBtn" type="submit">
               Submit
             </button>
           </form>
