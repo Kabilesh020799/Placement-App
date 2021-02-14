aea5220ccdfa2688ed8148c37952f551397f54a2
Kabilesh <kabilesh.ravi@cielocosta.com>
2021-02-14 20:17:31 +0530

    code cleaning

--- src/components/Admin/Dashboard/Dashboard.js
+++ src/components/Admin/Dashboard/Dashboard.js
@@ -63,36 +63,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-{
-  /* <div className="dashboard">
-      <div className="dashboard__sider">
-        <div
-          className="dashboard__siderItem"
-          style={{ marginTop: "50px" }}
-          onClick={() => setIsExisting(true)}
-          style={isExisting ? { background: "darkgray" } : null}
-        >
-          Schedules
-        </div>
-        <div
-          className="dashboard__siderItem"
-          onClick={() => setIsExisting(false)}
-        >
-          New Schedule
-        </div>
-      </div>
-      <div className="dashboard__main" style={{ background: "red" }}>
-        asdsdfdfsdfds
-        {/* <AdminCard /> */
-}
-{
-  /* {isExisting ? (
-            schedules.map((sch) => 
-              <AdminCard key={sch.id} />
-            )
-        ) : (
-          <div className="dashboard__main">
-            <AdminForm />
-          </div>
-        )} */
-}
